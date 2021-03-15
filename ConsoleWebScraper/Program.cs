ffabd9e2f4f275c7970f2aaece9f949cac4caa6d
katerynukr <kateryna_neholiuk@ukr.net>
2021-03-15 15:46:45 +0200

    first commit

---
+++ ConsoleWebScraper/Program.cs
@@ -0,0 +1,19 @@
+﻿using HtmlAgilityPack;
+using ScrapySharp.Network;
+using System;
+using System.Linq;
+
+namespace ConsoleWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://www.cvonline.lt/darbo-skelbimai/informacines-technologijos/";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            var coll = doc.DocumentNode.Descendants().Where(x => x.Name == "h2").ToList();
+            coll.ForEach(c => Console.WriteLine($"{c.SelectSingleNode("a").InnerText}"));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# WebScraper for work offers
+
+The is the console WebScraping project that searches and prints titles of work offers. The webpage used for the project https://www.cvonline.lt/darbo-skelbimai/informacines-technologijos.
+
+## Goal of this project :
+- practice and learn web scraping
+- practice linq expressions
+- learn and use HTMLAgilityPack Nuget package
+
